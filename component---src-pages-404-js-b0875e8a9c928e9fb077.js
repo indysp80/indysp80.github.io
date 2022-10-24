9f4fe5e534046021038b3fee0518d8fd2911dd4e
Serge Pesse <serge.pesse@gmail.com>
2022-10-24 07:54:37 +0200

    Updates

---
+++ component---src-pages-404-js-b0875e8a9c928e9fb077.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(t,e,n){n.r(e),n.d(e,{Head:function(){return l}});var u=n(7294),a=n(7075),r=n(9357),l=function(){return u.createElement(r.Z,{title:"404: Not Found"})};e.default=function(){return u.createElement(a.Z,null,u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b0875e8a9c928e9fb077.js.map
